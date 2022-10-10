69dfe74f0f72f6c40918b124769f8cd9746afaad
Michael Rosola <michael@roso.la>
2022-10-10 15:19:32 -0400

    test commit

--- SurfaceMaze/Program.cs
+++ SurfaceMaze/Program.cs
@@ -20,6 +20,9 @@ namespace SurfaceMaze
             //m.Build(new Point(42,42), true,new Point(15,15),5);
             MazeMaker m = new MazeMaker(bmp, 24, 1);
 
+
+
+
             m.Build(new Point(10, 17), false, new Point(10, 13), 5);
             m.Maze.Save(@"C:\Users\micha\OneDrive\Desktop\maze3.png",ImageFormat.Png);
 
